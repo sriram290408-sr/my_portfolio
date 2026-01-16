12a9ea0910608f78721e1848a81ae288e32675cd
sriramesakkimuthu-art <sriram290408@gmail.com>
2026-01-16 18:32:37 +0530

    fix in skills page

--- js/skills.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const bars = document.querySelectorAll(".progress-bar");
-
-  bars.forEach(bar => {
-    const level = bar.dataset.level;
-    bar.style.width = level + "%";
-  });
-});
